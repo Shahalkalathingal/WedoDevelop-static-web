b398b6b037ec016b03bb670f68a717628529d5f2
Shahal Kalathingal <shahal8002@gmail.com>
2021-12-23 12:34:54 +0530

    Update validate.js

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -4,31 +4,7 @@
   let forms = document.querySelectorAll('.php-email-form');
 
   forms.forEach( function(e) {
-    e.addEventListener('submit', function(event) {
-      event.preventDefault();
-      let name = document.getElementById('name')
-      let email = document.getElementById('email')
-      let subject = document.getElementById('subject')
-      let message = document.getElementById('message')
-
-      let thisForm = this;
-
-      let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-    
-      thisForm.querySelector('.loading').classList.add('d-block');
-      thisForm.querySelector('.error-message').classList.remove('d-block');
-      setTimeout(() => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        name.value = ''
-        email.value = ''
-        subject.value = ''
-        message.value = ''
-      }, 500);
-
-      
-     
-    });
+   
   });
 
 
